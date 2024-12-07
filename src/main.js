594c559d879ffd249c218921d027aab9b45b9d7e
LucasRealDev <Lucas.hpr.06@gmail.com>
2024-12-07 17:12:33 -0300

    teste de funcao

--- src/main.js
+++ src/main.js
@@ -1,28 +1,34 @@
 const sdk = require('node-appwrite');
 const moment = require('moment');
 
+// Criação do cliente do Appwrite
 const client = new sdk.Client();
-const database = new sdk.Database(client);
-const storage = new sdk.Storage(client);
 
+// Configuração do cliente
 client
   .setEndpoint(process.env.ENDPOINT) // Substitua com seu endpoint
   .setProject(process.env.PROJECT_ID) // Substitua com seu ID do projeto
   .setKey(process.env.API_KEY); // Substitua com sua chave de API
 
+// Acessando diretamente os serviços sem o 'new'
+const database = client.database;
+const storage = client.storage;
+
 module.exports = async (req, res) => {
+  console.log("teste");
   try {
-    // Corrigido para a nova forma de listagem de documentos
+    // Listar documentos da coleção 'stories'
     const response = await database.listDocuments(process.env.STORIES_COLLECTION_ID);
 
     for (const story of response.documents) {
       const storyCreatedAt = moment(story.$createdAt);
       const currentDate = moment();
 
+      // Verificar se o story tem mais de 1 dia
       if (currentDate.diff(storyCreatedAt, 'days') >= 1) {
-        // Apaga o arquivo no storage
+        // Apagar o arquivo no storage
         await storage.deleteFile(story.storyId);
-        // Apaga o documento da coleção
+        // Apagar o documento da coleção
         await database.deleteDocument(process.env.STORIES_COLLECTION_ID, story.$id);
 
         console.log(`Story com ID ${story.$id} apagado com sucesso.`);
